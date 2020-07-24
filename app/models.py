e09ddccbd5d15523108ea5e3fb95dda41abf4d01
danil <danil.mirtalibov.27@mail.ru>
2020-07-24 18:07:00 +0300

    Added a sqlite database app.db. Not wotking well YET.

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
           posts = db.relationship('Post', backref='author', lazy='dynamic')
 
           def __repr__(self):
-                    return '<User {}'.format(self.username)
+                    return '<User {}>'.format(self.username)
 
 
 class Post(db.Model):
@@ -19,4 +19,4 @@ class Post(db.Model):
           user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
           def __repr__(self):
-                    return '<Post {}'.format(self.body)
+                    return '<Post {}>'.format(self.body)
--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
           SECRET_KEY = os.environ.get('SECRET_KEY') or 'unicleSafe'
-          SQLALCHEMY_DATABASE_URI = os.environ.get('DTABASE_URL') or \'sqlite:///' + os.path.join(basedir, 'app.db')
-          SQLALCHEMY_TRACK_MODIFICATIONS = Flase
+          SQLALCHEMY_DATABASE_URI = os.environ.get('DTABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+          SQLALCHEMY_TRACK_MODIFICATIONS = False
