8d77ca6436cebcf7d155b4e444193aec68adcada
danil <danil.mirtalibov.27@mail.ru>
2020-07-24 15:06:00 +0300

    Importet SQLAlchemy module. Created models.py file. Added sqlite database named app.db .

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-          SECRET_KEY=os.environ.get('SECRET_KEY') or 'unicleSafe'
+          SECRET_KEY = os.environ.get('SECRET_KEY') or 'unicleSafe'
+          SQLALCHEMY_DATABASE_URI = os.environ.get('DTABASE_URL') or \'sqlite:///' + os.path.join(basedir, 'app.db')
+          SQLALCHEMY_TRACK_MODIFICATIONS = Flase
